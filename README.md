0b234432d7b9ad3186933c822ff868f1e59a2599
Your Name <you@example.com>
2025-04-03 21:26:57 -0300

    Refactoring Pila

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Tickets.h
 
 Daniel Gajardo ICD
+
 Tarea I - ICD2344-1 Estructuras de Datos - Prof. Ignacio Araya
 
 ---
 
-Sistema de gestión de tickets de soporte técnico
+## Sistema de gestión de tickets de soporte técnico
+
+### Descripción
 
 En esta tarea diseñarás una aplicación para administrar **tickets de soporte técnico** en una empresa, siguiendo un sistema de colas con prioridades. Un *ticket* representa una solicitud de ayuda ingresada por un usuario al sistema de soporte. Cada ticket contiene información relevante como un ID único, una descripción del problema, el nivel de prioridad (Alto, Medio o Bajo) y la hora de registro. Este sistema permite organizar y gestionar de forma eficiente las solicitudes de asistencia técnica, priorizando los casos más urgentes.
 
@@ -20,3 +23,57 @@ $ gcc tdas/*.c main.c -Wno-unused-result -Wall -Wextra -o main
 $ ./main
 ```
 
+---
+
+## Cómo compilar y ejecutar
+
+Este sistema ha sido desarrollado en lenguaje C y puede ejecutarse fácilmente utilizando Visual Studio Code junto con una extensión para C/C++, como C/C++ Extension Pack de Microsoft. Para comenzar a trabajar con el sistema en tu equipo local, sigue estos pasos:
+
+### Requisitos
+
+- VS CODE
+- Instalar la extensión **C/C++** (Microsoft).
+- Tener instalado un compilador de C (como **gcc**). Si estás en Windows, se recomienda instalar [MinGW](https://www.mingw-w64.org/) o utilizar el entorno [WSL](https://learn.microsoft.com/en-us/windows/wsl/).
+
+### Paso a paso
+
+- Abre el proyecto en Visual Studio Code
+    - Inicia Visual Studio Code.
+    - Selecciona Archivo > Abrir carpeta... y elige la carpeta donde descomprimiste el proyecto.
+- Compila el código
+    - Abre el archivo principal (por ejemplo, main.c).
+    - Abre la terminal integrada (Terminal > Nueva terminal).
+    - En la terminal, compila el programa con el siguiente comando 
+
+```shell
+$ gcc tdas/*.c main.c -Wno-unused-result -Wall -Wextra -o main
+```
+
+- Ejecuta el programa
+    - Una vez compilado, puedes ejecutar la aplicación con:
+
+```shell
+$ ./main
+```
+
+---
+
+## Funcionalidades
+
+### Funcionando correctamente:
+
+- Registrar pacientes con sus datos básicos y una prioridad inicial.
+- Asignar o modificar la prioridad de los pacientes.
+- Ver la lista de espera de pacientes, ordenada por prioridad y hora de registro.
+- Atender al siguiente paciente, respetando el orden de prioridad.
+
+#### Contribuciones
+
+### Problemas conocidos:
+
+- La funcionalidad para exportar e importar la lista de espera desde/hacia un archivo CSV aún está en desarrollo.
+
+### A mejorar:
+
+- Implementar una interfaz de usuario más amigable.
+- Permitir la edición de los datos de los pacientes.
--- main.c
+++ main.c
@@ -1,10 +1,14 @@
-#include "tdas/list.h"
+#include "tdas/queue.h"
 #include "tdas/extra.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define PRIORIDAD_DEFAULT 0 // Baja
 
 // Menú principal
-void mostrarMenuPrincipal() {
+void mostrarMenuPrincipal()
+{
   limpiarPantalla();
   puts("========================================");
   puts("     SISTEMA DE GESTION DE TICKETS");
@@ -14,75 +18,299 @@ void mostrarMenuPrincipal() {
   puts("2) Asignar prioridad a ticket");
   puts("3) Mostrar lista de espera");
   puts("4) Atender al siguiente ticket");
-  puts("5) Mostrar tickets por prioridad");
+  puts("5) Buscar ticket");
   puts("6) Salir");
 }
 
-typedef struct Ticket {
+typedef struct Tiempo
+{
+  int hora;
+  int minuto;
+  int segundo;
+  int dia;
+  int mes;
+  int anyo;
+} Tiempo;
+
+typedef struct Ticket
+{
   int id;
   char descripcion[100];
-  int prioridad; // [ Bajo: 1, Medio: 2, Alto: 3 ] default: 'B'
+  int prioridad; // [ Bajo: 0, Medio: 1, Alto: 2 ] default: 'B'
+  Tiempo tiempo;
 } Ticket;
 
-void registrar_ticket(List *tickets) {
-    limpiarPantalla();
-    printf("REGISTRAR NUEVO TICKET\n");
+Tiempo obtener_tiempo()
+{
+  time_t tiempo_actual = time(NULL);
+  struct tm *tiempo_info = localtime(&tiempo_actual);
 
-    Ticket *ticket = (Ticket *)malloc(sizeof(Ticket));
-    if (ticket == NULL) 
-        { puts("Error al registrar ticket"); return; }
+  Tiempo tiempo;
+  tiempo.hora = tiempo_info->tm_hour;
+  tiempo.minuto = tiempo_info->tm_min;
+  tiempo.segundo = tiempo_info->tm_sec;
 
-    printf("Ingrese el ID del ticket: ");
-    scanf("%d", &ticket->id);
-    printf("Ingrese la descripcion del ticket: \n");
-    scanf(" %s", ticket->descripcion);
-    ticket->prioridad = 'B';
+  tiempo.dia = tiempo_info->tm_mday;
+  tiempo.mes = tiempo_info->tm_mon + 1;
+  tiempo.anyo = tiempo_info->tm_year + 1900;
 
-    list_pushBack(tickets, ticket);
+  return tiempo;
 }
 
-void mostrar_lista_ticket(List *tickets) {
+void mostrar_ticket(Ticket *ticket)
+{
+  // Mostrar ticket con su id, descripcion y prioridad
+  // Diccionario de prioridades (N° -> Texto de Prioridad)
+  char *msg_prioridades[3] = {"Baja", "Media", "Alta"};
+  printf("ID: %d, Descripcion: %s, Prioridad: %s\n",
+         ticket->id, ticket->descripcion, msg_prioridades[ticket->prioridad]);
+}
 
-    limpiarPantalla();
-    puts("TICKETS EN ESPERA");  
-    puts("=================");
+void registrar_ticket(Queue *cola)
+{
+  limpiarPantalla();
+  printf("REGISTRAR NUEVO TICKET\n");
 
-    Ticket *ticket = (Ticket *)list_first(tickets);
-    while (ticket != NULL) {
-        printf("ID: %d, Descripcion: %s, Prioridad: %c\n", ticket->id, ticket->descripcion, ticket->prioridad);
-        ticket = (Ticket *)list_next(tickets);
+  Ticket *ticket = (Ticket *) malloc(sizeof(Ticket));
+  if (ticket == NULL)
+  {
+    puts("Error al registrar ticket");
+    return;
+  }
+
+  printf("Ingrese el ID del ticket: ");
+  scanf("%d", &ticket->id);
+  printf("Ingrese la descripcion del ticket: \n");
+  scanf(" %s", ticket->descripcion);
+
+  ticket->prioridad = PRIORIDAD_DEFAULT;
+  ticket->tiempo = obtener_tiempo();
+
+  queue_insert(cola, ticket);
+}
+
+void repoblar_cola(Queue *colaA, Queue *colaB)
+{
+  // Repoblar colaB con los tickets de colaA
+  Ticket *ticket;
+  while ((ticket = (Ticket *)queue_front(colaA)) != NULL)
+  {
+    queue_insert(colaB, ticket);
+    queue_remove(colaA);
+  }
+}
+
+void mostrar_lista_ticket(Queue *tickets_queues[])
+{
+
+  limpiarPantalla();
+  puts("TICKETS EN ESPERA");
+  puts("=================");
+
+  // Mostrar tickets ordenados por prioridad y tiempo
+  // Se recorre la cola de prioridad en orden descendente (Alta -> Media -> Baja)
+  for (int i = 2; i >= 0; i--)
+  {
+    Ticket *ticket;
+    Queue *queue_aux = queue_create();
+
+    while ((ticket = (Ticket *)queue_front(tickets_queues[i])) != NULL)
+    {
+      mostrar_ticket(ticket);
+      queue_insert(queue_aux, ticket);
+      queue_remove(tickets_queues[i]);
     }
 
-    // Mostrar ticket en la cola de espera
-    // printf("ticket en espera: \n");
-    // Aquí implementarías la lógica para recorrer y mostrar los ticket
+    repoblar_cola(queue_aux, tickets_queues[i]);
+    queue_clean(queue_aux);
+  }
 }
 
-int main() {
+int comparar_tickets(Ticket *ticket1, Ticket *ticket2)
+{
+
+  if (ticket1->tiempo.anyo != ticket2->tiempo.anyo)
+    return ticket1->tiempo.anyo > ticket2->tiempo.anyo;
+
+  if (ticket1->tiempo.mes != ticket2->tiempo.mes)
+    return ticket1->tiempo.mes > ticket2->tiempo.mes;
+
+  if (ticket1->tiempo.dia != ticket2->tiempo.dia)
+    return ticket1->tiempo.dia > ticket2->tiempo.dia;
+
+  if (ticket1->tiempo.hora != ticket2->tiempo.hora)
+    return ticket1->tiempo.hora > ticket2->tiempo.hora;
+
+  if (ticket1->tiempo.minuto != ticket2->tiempo.minuto)
+    return ticket1->tiempo.minuto > ticket2->tiempo.minuto;
+
+  if (ticket1->tiempo.segundo != ticket2->tiempo.segundo)
+    return ticket1->tiempo.segundo > ticket2->tiempo.segundo;
+
+  return 0;
+}
+
+void insertar_ticket(Queue *cola, Ticket *ticket)
+{
+  Ticket *ticket_aux;
+  Queue *queue_aux = queue_create();
+  while ((ticket_aux = (Ticket *)queue_front(cola)) != NULL)
+  {
+    if (comparar_tickets(ticket, ticket_aux))
+      queue_insert(queue_aux, ticket);
+    else
+      queue_insert(queue_aux, ticket_aux);
+
+    queue_remove(cola);
+  }
+
+  repoblar_cola(queue_aux, cola);
+  queue_clean(queue_aux);
+}
+
+void asignar_prioridad(Queue *tickets_queues[])
+{
+  limpiarPantalla();
+  puts("ASIGNAR PRIORIDAD A TICKET");
+  puts("=========================");
+
+  int id;
+  printf("Ingrese el ID del ticket: ");
+  scanf("%d", &id);
+
+  int nueva_prioridad;
+  printf("Ingrese la nueva prioridad (0: Bajo, 1: Medio, 2: Alto): ");
+  scanf("%d", &nueva_prioridad);
+
+  Ticket *ticket;
+  Queue *queue_aux = queue_create();
+
+  // Validar prioridad
+  if (nueva_prioridad < 0 || nueva_prioridad > 2)
+  {
+    puts("Prioridad no válida. Debe ser 0, 1 o 2.");
+    return;
+  }
+
+  // Buscar ticket recorriendo colas y asignar prioridad al encontrar ID
+  int flag = 0;
+  for (int i = 0; i < 3; i++)
+  {
+    flag = 0;
+    while ((ticket = (Ticket *)queue_front(tickets_queues[i])) != NULL)
+    {
+
+      if (ticket->id == id)
+      {
+        // Asignar prioridad y reorganizar cola
+        ticket->prioridad = nueva_prioridad;
+        insertar_ticket(tickets_queues[nueva_prioridad], ticket);
+        flag = 1;
+      }
+      else
+        queue_insert(queue_aux, ticket);
+
+      queue_remove(tickets_queues[i]);
+    }
+
+    repoblar_cola(queue_aux, tickets_queues[i]);
+    queue_clean(queue_aux);
+
+    if (flag == 1)
+      break;
+  }
+
+  if (flag == 0)
+    puts("Ticket no encontrado.");
+}
+
+void buscar_ticket(Queue *tickets_queues[])
+{
+  limpiarPantalla();
+  puts("BUSCAR TICKET");
+  puts("=============");
+
+  int id;
+  printf("Ingrese el ID del ticket: ");
+  scanf("%d", &id);
+
+  Ticket *ticket;
+  Queue *queue_aux = queue_create();
+  // Se recorren las colas de prioridad
+  for (int i = 0; i < 3; i++)
+  {
+    // Se recorren los tickets de la cola de prioridad
+    while ((ticket = (Ticket *)queue_front(tickets_queues[i])) != NULL)
+    {
+      // Si el ticket tiene el id buscado, se muestra
+      if (ticket->id == id)
+        mostrar_ticket(ticket);
+
+      queue_insert(queue_aux, ticket);
+      queue_remove(tickets_queues[i]);
+    }
+
+    repoblar_cola(queue_aux, tickets_queues[i]);
+    queue_clean(queue_aux);
+  }
+}
+
+void atender_ticket(Queue *tickets_queues[])
+{
+  limpiarPantalla();
+  puts("ATENDER TICKET");
+  puts("=============");
+
+  Ticket *ticket;
+  int flag = 0;
+
+  for (int i = 2; i >= 0; i--)
+  {
+    ticket = (Ticket *)queue_front(tickets_queues[i]);
+    if (ticket != NULL)
+    {
+      mostrar_ticket(ticket);
+      puts("Ticket atendido correctamente.");
+      queue_remove(tickets_queues[i]);
+      flag = 1;
+      break;
+    }
+  }
+
+  if (flag == 0)
+    puts("No hay tickets en espera.");
+}
+
+int main()
+{
+  // Colas de tickets (Baja, Media, Alta)
+  Queue *tickets_queues[3] = {queue_create(), queue_create(), queue_create()};
+
+  // Bucle principal
   char opcion;
-  List *tickets = list_create(); // puedes usar una lista para gestionar los ticket
-
-  do {
+  do
+  {
     mostrarMenuPrincipal();
-    printf("Ingrese su opcion: ");
-    scanf(" %c", &opcion); // Nota el espacio antes de %c para consumir el
-                           // newline anterior
 
-    switch (opcion) {
+    printf("Ingrese su opcion: ");
+    scanf(" %c", &opcion);
+
+    switch (opcion)
+    {
     case '1':
-      registrar_ticket(tickets);
+      registrar_ticket(tickets_queues[PRIORIDAD_DEFAULT]);
       break;
     case '2':
-      // Lógica para asignar prioridad
+      asignar_prioridad(tickets_queues);
       break;
     case '3':
-      mostrar_lista_ticket(tickets);
+      mostrar_lista_ticket(tickets_queues);
       break;
     case '4':
-      // Lógica para atender al siguiente ticket
+      atender_ticket(tickets_queues);
       break;
     case '5':
-      // Lógica para mostrar ticket por prioridad
+      buscar_ticket(tickets_queues);
       break;
     case '6':
       puts("Saliendo del sistema de gestión hospitalaria...");
@@ -95,7 +323,8 @@ int main() {
   } while (opcion != '6');
 
   // Liberar recursos, si es necesario
-  list_clean(tickets);
+  for (int i = 0; i < 3; i++)
+    queue_clean(tickets_queues[i]);
 
   return 0;
 }
